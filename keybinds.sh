4ee9b3aca91a96bfc36cc6d6b20671b02e1d53c3
mat <matdallen@gmail.com>
2024-07-05 23:47:40 -0700

    added always produce

--- keybinds.sh
+++ keybinds.sh
@@ -21,9 +21,6 @@ set_custom_keybind() {
     local command=$3
     local bind=$4
 
-#    if gsettings list-recursively | grep -q "$key_combination"; then
-#        echo "Keybinding already set for: $bind"
-#    else
 	echo "binding $name to $bind..."
     gsettings set "${MEDIA_KEYS}.custom-keybinding:${KEYBIND_DIR}/custom${index}/" name "${name}"
     gsettings set "${MEDIA_KEYS}.custom-keybinding:${KEYBIND_DIR}/custom${index}/" command "${command}"
@@ -33,43 +30,46 @@ set_custom_keybind() {
 }
 
 run_it() {
-    create_slots 40
+    create_slots 50
+    always_produce_link="https://docs.google.com/document/d/1_UXIxwMgg-RK79POgb7FQQQjb0CJbbKejt9iff1rhFw/edit"
 
     ## comms
-    set_custom_keybind 1  "open FB Msgr"      "firefox https://www.facebook.com/messages/"                                    "<Super>F"
-    set_custom_keybind 2  "open sms msgr"     "firefox https://messages.google.com/web/conversations"                         "<Super><Alt>X"
-    set_custom_keybind 3  "open Gmail"        "firefox https://mail.google.com/mail/u/0/"                                     "<Super>G"
-    set_custom_keybind 4  "fb_mess"           "firefox https://www.facebook.com/messages/"                                    "<Super>M"
-    set_custom_keybind 5  "linkedin"           "firefox https://www.linkedin.com/feed/"                                       "<Super><Alt>L"
+    set_custom_keybind 1  "open FB Msgr"       "firefox https://www.facebook.com/messages/"                                            "<Super>F"
+    set_custom_keybind 2  "open sms msgr"      "firefox https://messages.google.com/web/conversations"                                 "<Super><Alt>X"
+    set_custom_keybind 3  "open Gmail"         "firefox https://mail.google.com/mail/u/0/"                                             "<Super>G"
+    set_custom_keybind 4  "fb_mess"            "firefox https://www.facebook.com/messages/"                                            "<Super>M"
+    set_custom_keybind 5  "linkedin"           "firefox https://www.linkedin.com/feed/"                                                "<Super><Alt>L"
 
     ## search / info
-    set_custom_keybind 10  "ChattyG"           "firefox https://chatgpt.com/?model=gpt-4"                                     "<Super><Shift>O"
-    set_custom_keybind 11  "ClaudeBae"         "firefox https://claude.ai/chats"                                              "<Super><Shift>A"
-    set_custom_keybind 12  "searchPanel"       "/home/mat/Documents/ProgramExperiments/searchPanel/searchPanel"               "<Shift><Super>S"
-    set_custom_keybind 13  "gCal"              "firefox https://calendar.google.com/"                                         "<Super>C"
-    set_custom_keybind 14  "new gDoc"          "firefox https://docs.new/"                                                    "<Super><Shift>D"
-    set_custom_keybind 15  "openLink"          "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/open_run.sh"            "<Ctrl><Shift>L"
-    set_custom_keybind 16  "quote"             "python3 /home/mat/Documents/ProgramExperiments/.setup/hotkeys/quote.py"       "<Ctrl><Shift>U"
+    set_custom_keybind 10  "ChattyG"           "firefox https://chatgpt.com/?model=gpt-4"                                              "<Super><Shift>O"
+    set_custom_keybind 11  "ClaudeBae"         "firefox https://claude.ai/chats"                                                       "<Super><Shift>A"
+    set_custom_keybind 12  "searchPanel"       "/home/mat/Documents/ProgramExperiments/searchPanel/searchPanel"                        "<Super><Shift>S"
+    set_custom_keybind 13  "gCal"              "firefox https://calendar.google.com/"                                                  "<Super>C"
+    set_custom_keybind 14  "new gDoc"          "firefox https://docs.new/"                                                             "<Super><Shift>D"
+    set_custom_keybind 15  "openLink"          "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/open_run.sh"                     "<Ctrl><Shift>L"
+    set_custom_keybind 16  "quote"             "python3 /home/mat/Documents/ProgramExperiments/.setup/hotkeys/quote.py"                "<Ctrl><Shift>U"
 
     ## web services
-    set_custom_keybind 20  "fact2BEEF"         "/home/mat/Documents/ProgramExperiments/fact2cloze/fact2clozeBEEF.sh"          "<Alt><Ctrl>C"
-    set_custom_keybind 21  "ss2text"           "/home/mat/Documents/ProgramExperiments/ss2txt/ss.sh"                          "<Ctrl><Shift>S"
-    set_custom_keybind 22  "perpSearch"        "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/perplexity.search"      "<Alt><Shift>P"
-    set_custom_keybind 23  "perpSelSearch"     "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/perplexitySel.search"   "<Alt><Shift>O"
-    set_custom_keybind 24  "STTpb"             "/home/mat/Documents/ProgramExperiments/speech2txt_hk/main.sh"                 "<Alt><Ctrl>R"
-    set_custom_keybind 25  "open maps"         "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/open_maps.sh"           "<Super><Alt>M"
-    set_custom_keybind 26  "open partiful"     "firefox https://partiful.com/events"                                          "<Super><Alt>P"
-    set_custom_keybind 27  "aquatic ambi"      "firefox https://www.youtube.com/watch?v=CKAc3nYEatw"                          "<Super><Shift>Q"
-    set_custom_keybind 28  "2048"              "firefox https://www.2048.org/"                                                "<Ctrl><Shift>2"
-    set_custom_keybind 29  "yoga"              "firefox https://www.bodytempyoga.com/schedule"                                "<Ctrl><Shift>B"
+    set_custom_keybind 20  "fact2BEEF"         "/home/mat/Documents/ProgramExperiments/fact2cloze/fact2clozeBEEF.sh"                   "<Ctrl><Alt>C"
+    set_custom_keybind 21  "ss2text"           "/home/mat/Documents/ProgramExperiments/ss2txt/ss.sh"                                   "<Ctrl><Shift>S"
+    set_custom_keybind 22  "perpSearch"        "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/perplexity.search"               "<Alt><Shift>P"
+    set_custom_keybind 23  "perpSelSearch"     "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/perplexitySel.search"            "<Alt><Shift>O"
+    set_custom_keybind 24  "STTpb"             "/home/mat/Documents/ProgramExperiments/speech2txt_hk/main.sh"                          "<Ctrl><Alt>R"
+    set_custom_keybind 25  "open maps"         "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/open_maps.sh"                    "<Super><Alt>M"
+    set_custom_keybind 26  "open partiful"     "firefox https://partiful.com/events"                                                   "<Super><Alt>P"
+    set_custom_keybind 27  "aquatic ambi"      "firefox https://www.youtube.com/watch?v=CKAc3nYEatw"                                   "<Super><Shift>Q"
+    set_custom_keybind 28  "2048"              "firefox https://www.2048.org/"                                                         "<Ctrl><Shift> 2"
+    set_custom_keybind 29  "yoga"              "firefox https://www.bodytempyoga.com/schedule"                                         "<Ctrl><Shift>B"
+
+    set_custom_keybind 30  "Always produce"    "firefox $always_produce_link"                                                          "<Super><Shift>P"
 
     ## sys services
-    set_custom_keybind 30  "toggle Bluetooth"  "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/btPairing.sh"           "<Shift><Alt>B"
-    set_custom_keybind 31  "fleetNotes"        "/home/mat/Documents/ProgramExperiments/fleetingNotes/main/fleetWindow.sh"     "<Alt><Ctrl>F"
+    set_custom_keybind 40  "toggle Bluetooth"  "/home/mat/Documents/ProgramExperiments/.setup/hotkeys/btPairing.sh"                    "<Alt><Shift>B"
+    set_custom_keybind 41  "fleetNotes"        "/home/mat/Documents/ProgramExperiments/fleetingNotes/main/fleetWindow.sh"              "<Ctrl><Alt>F"
 
 }
 
 run_it
 
 # something to look into here is whether or not I want to overwrite the stock keybinds that Ubuntu comes with. I'd like to have <Super> be avail by itself as a prefix
-# But if it's a lot of work I'll do something else. It'd be nice to have that flexibility. Juice may not be worth the squeeze.
+#### But if it's a lot of work I'll do something else. It'd be nice to have that flexibility. Juice may not be worth the squeeze.
